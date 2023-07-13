9d523215aed17620b1b9fdb825f109d2f704e375
Devesh Kedia <69102825+deveshkedia@users.noreply.github.com>
2023-07-13 20:40:18 +0530

    seller panal

--- ecom/admin.py
+++ ecom/admin.py
@@ -1,6 +1,7 @@
 from typing import Any
 from django.contrib import admin
 from django.db.models.query import QuerySet
+import datetime
 from django.http.request import HttpRequest
 from .models import *
 
@@ -31,14 +32,12 @@ admin.site.register(Tracking)
 admin.site.register(category)
 admin.site.register(Section)
 
-
 admin.site.register(banner_area)
 admin.site.register(h1banner)
 admin.site.register(h2banner)
 admin.site.register(sub_category)
 admin.site.register(main_category)
 admin.site.register(Coupon_Code)
-admin.site.register(Checkout)
 admin.site.register(Productsimageurl)
 admin.site.register(Additional_Information)
 admin.site.register(size)
@@ -55,8 +54,31 @@ class PaymentInline(admin.StackedInline):
     
 class OrderAdmin(admin.ModelAdmin):
     inlines = [ItemInline,PaymentInline]
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        print(qs)
+        flag=False
+        for i in qs:
+            for prod in i.items.all():
+                if prod.product.seller == request.user:
+                    flag=True
+            if flag==False:
+                qs = qs.exclude(id=i.id)
+    
+        qs = qs.order_by('-order_date')
+        return qs
     def has_change_permission(self, request, obj=None):
         return False
-admin.site.register(Payment)
+    
+class PaymentAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return None
+admin.site.register(Checkout,PaymentAdmin)
+admin.site.register(Payment,PaymentAdmin)
 admin.site.register(Order,OrderAdmin)
 admin.site.register(Notification)
