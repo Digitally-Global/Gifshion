cfa30ffec3a41ab8d0b10c178f58d83e69e06849
Devesh Kedia <69102825+deveshkedia@users.noreply.github.com>
2023-07-20 20:28:18 +0530

    daatabase

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6)luf6o*#hhonf%y4nf_^-lp_g_$@pxe#e)vw7c@^*kk@fp80#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = ['*']
 
@@ -103,23 +103,35 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# if not DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': 'gifshion',
+#             'USER': 'gifshion',
+#             'PASSWORD': 'gifshion123',
+#             'HOST': 'my-database.c9hrhzydvbtk.ap-south-1.rds.amazonaws.com',
+#             'PORT': '5432',
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gifshion',
+        'USER': 'gifshion',
+        'PASSWORD': 'gifshion123',
+        'HOST': 'my-database.c9hrhzydvbtk.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'gifshion',
-#         'USER': 'gifshion',
-#         'PASSWORD': 'gifshion123',
-#         'HOST': 'my-database.c9hrhzydvbtk.ap-south-1.rds.amazonaws.com',
-#         'PORT': '5432',
-#     }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
