33698312589df75d3c648a36ad48da051154d840
Devesh Kedia <69102825+deveshkedia@users.noreply.github.com>
2023-08-03 22:16:32 +0530

    hellko

--- app/urls.py
+++ app/urls.py
@@ -81,3 +81,7 @@ urlpatterns = [
 
     
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Gifshion"
+admin.site.site_title = "Gifshion Portal"
+admin.site.index_title = "Welcome to Gifshion  Portal"
